daf72ed85eab49045c4df0824b43fddacc9120e0
Vlad Hurko <vlad22.2000@gmail.com>
2025-08-12 18:14:51 +0100

    Solution

--- db/models.py
+++ db/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+
+class Race(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=255)
+    bonus = models.CharField(max_length=255)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+
+
+class Guild(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=255)
+    bio = models.TextField(max_length=255)
+    race =models.ForeignKey(Race, on_delete=models.CASCADE)
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL)
+    created_add = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-import init_django_orm  # noqa: F401
-
-from db.models import Race, Skill, Player, Guild
-
-
-def main() -> None:
-    pass
+import json
+from db.models import Race, Skill, Guild, Player
 
 
-if __name__ == "__main__":
-    main()
+def main():
+    with open("players.json", "r", encoding="utf-8") as f:
+        players_data = json.load(f)
+
+    for pdata in players_data:
+        race_obj, _ = Race.objects.get_or_create(
+            name=pdata["race"]["name"],
+            defaults={"description": pdata["race"].get("description", "")}
+        )
+
+        for skill in pdata["race"].get("skills", []):
+            Skill.objects.get_or_create(
+                name=skill["name"],
+                defaults={
+                    "bonus": skill["bonus"],
+                    "race": race_obj
+                }
+            )
+
+        guild_obj = None
+        if pdata.get("guild"):
+            guild_obj, _ = Guild.objects.get_or_create(
+                name=pdata["guild"]["name"],
+                defaults={"description": pdata["guild"].get("description")}
+            )
+
+        Player.objects.get_or_create(
+            nickname=pdata["nickname"],
+            defaults={
+                "email": pdata["email"],
+                "bio": pdata["bio"],
+                "race": race_obj,
+                "guild": guild_obj,
+            }
+        )
