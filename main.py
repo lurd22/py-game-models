bf99536abd2e492f73c9192c3280934d16cb44a9
Vlad Hurko <vlad22.2000@gmail.com>
2025-08-12 18:24:36 +0100

    Solution

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ def main() -> None:
     with open("players.json", "r", encoding="utf-8") as f:
         players_data = json.load(f)
 
-    for pdata in players_data:
+    for nickname, pdata in players_data.items():
+        race_info = pdata["race"]
         race_obj, _ = Race.objects.get_or_create(
-            name=pdata["race"]["name"],
-            defaults={"description": pdata["race"].get("description", "")}
+            name=race_info["name"],
+            defaults={"description": race_info.get("description", "")}
         )
 
-        for skill in pdata["race"].get("skills", []):
+        for skill in race_info.get("skills", []):
             Skill.objects.get_or_create(
                 name=skill["name"],
                 defaults={
@@ -23,13 +24,14 @@ def main() -> None:
 
         guild_obj = None
         if pdata.get("guild"):
+            guild_info = pdata["guild"]
             guild_obj, _ = Guild.objects.get_or_create(
-                name=pdata["guild"]["name"],
-                defaults={"description": pdata["guild"].get("description")}
+                name=guild_info["name"],
+                defaults={"description": guild_info.get("description")}
             )
 
         Player.objects.get_or_create(
-            nickname=pdata["nickname"],
+            nickname=nickname,
             defaults={
                 "email": pdata["email"],
                 "bio": pdata["bio"],
